f56aaf0cae42c7fd119438303b5572f4dbd1417d
xchoavagyan <57505023+xchoavagyan@users.noreply.github.com>
2021-07-28 19:42:47 +0400

    Update Ec2AwsApplicationTests.java

--- src/test/java/com/example/ec2_aws/Ec2AwsApplicationTests.java
+++ src/test/java/com/example/ec2_aws/Ec2AwsApplicationTests.java
@@ -3,11 +3,11 @@ package com.example.ec2_aws;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class Ec2AwsApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//@SpringBootTest
+//class Ec2AwsApplicationTests {
+//
+  //  @Test
+   // void contextLoads() {
+    //}
+//
+//}
